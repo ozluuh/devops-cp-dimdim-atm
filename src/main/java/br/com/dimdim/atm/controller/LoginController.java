87777ca59f8874e767434e21dcb9268b6b28fd0a
ozluuh <ozluuh@gmail.com>
2021-09-06 23:42:30 -0300

    refactor: add validation in login

--- src/main/java/br/com/dimdim/atm/controller/LoginController.java
+++ src/main/java/br/com/dimdim/atm/controller/LoginController.java
@@ -1,5 +1,7 @@
 package br.com.dimdim.atm.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,21 +15,34 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Controller
-@RequestMapping(path = { "/", "/login" })
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class LoginController {
 
+	private static final String CUSTOMER_ATTRIBUTE = "customer";
+
 	private final CustomerRepository repo;
 
-    @GetMapping
-    public String index(final Customer customer) {
-        return "login";
-    }
+	@GetMapping(path = { "/", "/login" })
+	public String index(HttpSession session) {
+		if (session.getAttribute(CUSTOMER_ATTRIBUTE) != null) {
+			session.removeAttribute(CUSTOMER_ATTRIBUTE);
+			log.info("hasCustomerOnLogin: Attribute Removed");
 
-    @PostMapping
-    public String home(final Customer customer) {
-		log.info("Received: {}", customer);
+			session.setAttribute("error", true);
+			session.setAttribute("errorTitle", "Sessão expirada");
+			session.setAttribute("errorMessage", "Realize o acesso novamente.");
+		}
+		return "login";
+	}
 
-        return "index";
-    }
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		if (!session.isNew()) {
+			session.invalidate();
+			log.info("Session invalidated");
+		}
+
+		return "redirect:/login";
+	}
 }
