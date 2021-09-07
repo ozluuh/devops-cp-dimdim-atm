b502002381d5a795c96d08f3e18249ab01188cf8
ozluuh <ozluuh@gmail.com>
2021-09-07 02:03:35 -0300

    refactor: add description to account type

--- src/main/java/br/com/dimdim/atm/model/AccountType.java
+++ src/main/java/br/com/dimdim/atm/model/AccountType.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AccountType {
-    SPECIAL(500d), PREMIUM(1000d), DEFAULT(100d);
+	SPECIAL(500d, "Especial"), PREMIUM(1000d, "Premium"), DEFAULT(100d, "Padrão");
 
-    private final Double limit;
+	private final Double limit;
+
+	private final String display;
 }
